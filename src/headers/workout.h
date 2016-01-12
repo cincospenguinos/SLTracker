760b43c9665bada6383a6e8a1211a57b9c1e6bba
Andre LaFleur <andre.lafleur@utah.edu>
2016-01-11 23:50:37 -0700

    Add implementation of workout.h

--- src/headers/workout.h
+++ src/headers/workout.h
@@ -6,7 +6,7 @@
  */
 
 /* Workout struct. */
-typedef struct {
+typedef struct Workout{
   uint16_t weight[3]; // Weight for each exercise
 
   uint8_t ex_sets1[5]; // exercise sets - number between 0 and 5 for each index
@@ -18,7 +18,7 @@ typedef struct {
   int year; // Number of years since 1900
 
   bool day_type; // False = B day, ALWAYS.
-} Workout;
+}__attribute__((__packed__)) Workout;
 
 /* Creates a workout with today's date. */
 Workout workout_create_today();
@@ -28,3 +28,9 @@ const char * get_exercise_name(Workout *, uint8_t);
 
 /* Returns a workout at the index requested. */
 Workout get_workout_at(int);
+
+/* Stores the workout at the next available space in persistent data. */
+void store_workout(Workout);
+
+/* Initializes the persistent data setup */
+void initialize_persistent_data();
