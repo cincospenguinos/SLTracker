006fcaeea90247016d10afbefc1536b454ce2473
Andre LaFleur <andre.lafleur@utah.edu>
2016-01-11 23:22:25 -0700

    Add config.c file.

---
+++ src/config.c
@@ -0,0 +1,14 @@
+/**
+ * config.c
+ *
+ * Configuration functions to manage the app's configuration.
+ */
+#pragma once
+#include <pebble.h>
+
+#define VERSION_KEY 1
+#define LANGUAGE_KEY 2
+
+float get_installed_version(){
+  return 0.0; // TODO: This
+}
